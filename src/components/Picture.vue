bbc834eb5de2e21caef3cc612465ff262d356063
Biying Zhang <biz11@pitt.edu>
2020-05-19 00:08:36 -0400

    Update on Picture component to view in full size

--- src/components/Picture.vue
+++ src/components/Picture.vue
@@ -1,8 +1,12 @@
 <template>
     <transition  appear enter-active-class="animate__animated animate__rollIn delay">
-    <div class="pic">
+    <div class="pic" @click="showFull = !showFull">
         <img :src="url" />
         <p>{{description}}</p>
+        <div v-if="showFull" class="page-container"> 
+            <span class="page-container black-background"></span>
+            <div class="full-pic"><img :src="url"/></div>
+        </div>
     </div>
     </transition>
 </template>
@@ -14,6 +18,11 @@ export default {
         name: String,
         description: String
     },
+    data() {
+        return {
+            showFull: false
+        }
+    },
     computed: {
         url() {
             return require('../assets/' + this.name + '.jpg')
@@ -23,7 +32,7 @@ export default {
 </script>
 
 <style scoped>
- img {
+img {
   max-width: 95%;
   max-height: 95%;
 }
@@ -33,5 +42,24 @@ export default {
 }
 .delay{
     animation-delay: 0.8s;
+}
+.page-container{
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+}
+.black-background{
+    background-color: black;
+    opacity: 0.8;
+}
+.full-pic{
+    position: sticky;
+    top: 0;
+}
+.full-pic  img {
+    max-height: 100vh;
+    max-width: 100vw;
 }
 </style>
